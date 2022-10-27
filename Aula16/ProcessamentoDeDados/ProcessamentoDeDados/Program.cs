4f5e116fa7c98766a8a66f976ab5342fd1a62c4c
Lucas <lucasandim@yahoo.com.br>
2022-10-26 22:00:06 -0400

    curso

---
+++ Aula16/ProcessamentoDeDados/ProcessamentoDeDados/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace curso
+{
+    class  Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+
+            x = 5;
+
+            y = 2 * x;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+            Console.ReadLine();
+        }
+    }
+}
+
