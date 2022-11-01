c331d8149a403781e553187d1286631e10a46942
Lucas <lucasandim@yahoo.com.br>
2022-11-01 19:47:31 -0400

    aula21

---
+++ Aula21/Aula21/Aula21/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Globalization;
+
+// Debug ou execução passo a passo
+// para definir a linha que vai iniciar click em qualquer lugar da linha -> vá em depurar -> Alterar ponto de interrupção
+// ferramenta incrivel para identificar erros, em caixa que abre na parte inferior esquerda você pode navegar e entender o que ta acontecendo com cada variavel.
+// torna muito facil acompanhar o comportamento interno de cada variavel.
