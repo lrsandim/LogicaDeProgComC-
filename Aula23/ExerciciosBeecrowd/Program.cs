db7eb2afe2c39f6af6f8eb486fd50474de9f66ab
Lucas <lucasandim@yahoo.com.br>
2022-11-07 16:43:53 -0400

    aula23

---
+++ Aula23/ExerciciosBeecrowd/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, r, n = 3.14159;
+
+            r = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            a = n * Math.Pow(r, 2.0);
+
+            Console.WriteLine("A=" + a.ToString("F4", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+        }
+    }
+}
