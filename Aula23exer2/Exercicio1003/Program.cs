7ddd7555d8f8056dc041e205879b9efde03e10da
Lucas <lucasandim@yahoo.com.br>
2022-11-08 15:49:59 -0400

    exercicioProposto2

---
+++ Aula23exer2/Exercicio1003/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, X;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+
+            X = A + B;
+
+            Console.WriteLine("SOMA = " + X);
+
+            Console.ReadLine();
+        }
+
+    }
+}
