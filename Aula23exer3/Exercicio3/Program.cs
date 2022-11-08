f2d7caac9f97a867c6b94c08158cdc4eabf1cd92
Lucas <lucasandim@yahoo.com.br>
2022-11-08 16:10:10 -0400

    exercicio4

---
+++ Aula23exer3/Exercicio3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, PROD;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+
+            PROD = A * B;
+
+            Console.WriteLine("PROD = " + PROD);
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
---
+++ Aula23exer4/Exercicio4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C, D, dif;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            C = int.Parse(Console.ReadLine());
+            D = int.Parse(Console.ReadLine());
+
+            dif = A * B - C * D;
+
+            Console.WriteLine("DIFERENCA = " + dif);
+
+            Console.ReadLine();
+        }
+    }
+}
