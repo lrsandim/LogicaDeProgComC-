b25dad0c7afa57a670f1d6caba80711b5d55cba2
Lucas <lucasandim@yahoo.com.br>
2022-11-09 20:36:24 -0400

    aula23exer1010

---
+++ Aula23exer1010/Exercicio1010/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] peca1 = Console.ReadLine().Split(' ');
+            int codigoPeca = int.Parse(peca1[0]);
+            int numeroDePecas = int.Parse(peca1[1]);
+            double valorUnitario = double.Parse(peca1[2], CultureInfo.InvariantCulture);
+
+            string[] peca2 = Console.ReadLine().Split(' ');
+            int codigoPeca2 = int.Parse(peca2[0]);
+            int numeroDePecas2 = int.Parse(peca2[1]);
+            double valorUnitario2 = double.Parse(peca2[2], CultureInfo.InvariantCulture);
+
+            double valorAPgar = (numeroDePecas * valorUnitario) + (numeroDePecas2 * valorUnitario2);
+
+            Console.WriteLine("VALOR A PAGAR: R$ " + valorAPgar.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Aula23exer5/Exercicio5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio5
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            double numero, horas, recebe, salario;
+
+            numero = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            horas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            recebe = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            salario = horas * recebe;
+
+            Console.WriteLine("NUMBER = " + numero);
+            Console.WriteLine("SALARY = U$ " + salario.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+        }
+    }
+}
