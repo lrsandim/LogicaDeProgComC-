2d047d8c12ec118f275a87fa4616d9511d88d318
Lucas <lucasandim@yahoo.com.br>
2023-01-14 18:37:04 -0400

    Aula35

---
+++ Aula35/Aula35/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+
+            String[] vet = Console.ReadLine().Split(' ');
+
+            x = int.Parse(vet[0]);
+            y = int.Parse(vet[1]);
+            z = int.Parse(vet[2]);
+
+            if (x < y && x < z)
+            {
+                Console.WriteLine("MENOR = " + x);
+            }
+            else
+            {
+                if (y < z)
+                {
+                    Console.WriteLine("MENOR = " + y);
+                }
+                else
+                {
+                    Console.WriteLine("MENOR = " + z);
+                }
+            }
+        }
+    }
+}
