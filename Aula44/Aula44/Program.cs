10a7d9eea4b48175030cd4996997abfa66d079a0
Lucas <lucasandim@yahoo.com.br>
2023-01-16 09:55:33 -0400

    aula44

---
+++ Aula44/Aula44/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Net.Http.Headers;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double idade, soma, media;
+            int cont;
+
+            idade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            soma = 0.0;
+            cont = 0;
+            while (idade >= 0)
+            {
+                soma = soma + idade;
+                cont = cont + 1;
+                idade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            if (cont == 0)
+            {
+                Console.WriteLine("impossivel calcular");
+            }
+            else
+            {
+                media = soma / cont;
+                Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+                Console.ReadLine();
+            }
+        }
+    }
+}
