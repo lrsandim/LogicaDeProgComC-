6cc7319b3e89be412fba1372df669b9d382d6d7f
Lucas <lucasandim@yahoo.com.br>
2023-01-16 08:23:52 -0400

    aula37

---
+++ Aula37/Aula37/Aula37/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            string dia;
+
+            if (x == 1)
+            {
+                dia = "domingo";
+            }
+            else if (x == 2)
+            {
+                dia = "segunda";
+            }
+            else if (x == 3)
+            {
+                dia = "terça";
+            }
+            else if (x == 4)
+            {
+                dia = "quarta";
+            }
+            else if (x == 5)
+            {
+                dia = "quinta";
+            }
+            else if (x == 6)
+            {
+                dia = "sexta";
+            }
+            else if (x == 7)
+            {
+                dia = "sábado";
+            }
+            else
+            {
+                dia = "valor inválido";
+            }
+
+            Console.WriteLine("Dia da semana: " + dia);
+            Console.ReadLine();
+        }
+    }
+}
